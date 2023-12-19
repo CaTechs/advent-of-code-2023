d20ff676eea6575779e886ff2043544361b1a27a
CaTechs <null>
2023-12-19 12:37:46 +0100

    Day 18, repasse avec de plus jolis algo prévus à cet effet

--- day18Shoe.py
+++ day18Shoe.py
@@ -30,10 +30,19 @@ def day18Shoe(suf):
 		pos = (pos[0] + dis*d[0], pos[1] + dis*d[1])
 		listPos.append(pos)
 	area = 0
+	# Calcul du Shoelace theorem, Trapezoid formula
 	for i in rLen(listPos):
 		x1, x2 = listPos[i], listPos[(i+1) % len(listPos)]
 		area += (x1[1] + x2[1])*(x1[0] - x2[0])
-	return abs(area // 2) + bord // 2 + 1
+	area = abs(area // 2) #Résultat du ShoeLace theorem
+
+	# De là, on passe au théorème de Pick, qui nous dit
+	# innerPoint + (outerPoint)//2 - 1 = area (que l'on vient de calculer)
+	# Et nous on cherche innerPoint + outerPoint (noté I + O)
+	# D'où I = area - O/2 + 1
+	# I + O = area + O/2 + 1
+	# Et donc, on arrive à ce return
+	return area + bord // 2 + 1
 res = day18Shoe("t")
 print(res)
 realRes = day18Shoe("")
